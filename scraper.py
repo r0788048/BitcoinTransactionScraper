dea4046de786be156523d63ab055e80db7a1f28d
r0788048 <79154692+r0788048@users.noreply.github.com>
2021-02-16 16:08:23 +0100

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+
+def scrape():
+    url = 'https://www.blockchain.com/btc/unconfirmed-transactions'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, features="html.parser")
+    #print(soup.prettify())
+
+    has = soup.find_all('a',{ "class" : "sc-1r996ns-0 fLwyDF sc-1tbyx6t-1 kCGMTY iklhnl-0 eEewhk d53qjk-0 ctEFcK"})
+    allebs = soup.find_all('span',{ "class" : "sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC"})
+
+    #for x in range(len(allebs)):
+    #    if(x%3 == 0):
+    #        print(allebs[x].text)
+    #    if(x%3 == 1):
+    #        print(allebs[x].text[:-4])
+    #    if(x%3 == 2):
+    #        print(allebs[x].text[1:])
+
+    tel = 0
+    for x in range(0, len(allebs)):
+        if(x%3 == 1):
+            if (float(allebs[x].text[:-4]) > float(allebs[tel].text[:-4])):
+                tel = x
+
+    print(has[(tel-1)%3].text, allebs[tel-1].text, allebs[tel].text, allebs[tel+1].text)
+
+while True:
+    scrape()
+    time.sleep(60)
