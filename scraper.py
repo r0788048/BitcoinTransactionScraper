75871335435a8d7df9a860a3636aae6fbcfce7bf
r0788048 <79154692+r0788048@users.noreply.github.com>
2021-02-21 23:56:06 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-
 def scrape():
     url = 'https://www.blockchain.com/btc/unconfirmed-transactions'
     r = requests.get(url)
@@ -26,11 +25,24 @@ def scrape():
             if (float(allebs[x].text[:-4]) > float(allebs[tel].text[:-4])):
                 tel = x
     
-    string = has[(tel-1)%3].text + ' ' + allebs[tel-1].text + ' ' + allebs[tel].text + ' ' + allebs[tel+1].text
+    string = has[(tel-1)%3].text + ' | ' + allebs[tel-1].text + ' | ' + allebs[tel].text + ' | ' + allebs[tel+1].text
     f = open("LogBitcoin.txt", "a")
     f.write("\n")
     f.write(string)
     f.close()
+
+try:
+    f = open("LogBitCoin.txt", "x")
+    f.close()
+    f = open("LogBitcoin.txt", "a")
+    f.write("BitcoinTransactionScraper Logfile")
+    f.write("\n")
+    f.write("HASH | TIME | AM_BTC | AM_USD")
+    f.write("\n")
+    f.write("==============================================")
+    f.close()
+except:
+    print("File already exists. Adding new logs.")
 
 while True:
     scrape()
