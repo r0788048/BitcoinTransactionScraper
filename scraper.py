9320ed3c299fb96e638fe49613d7efabfe24c76a
r0788048 <79154692+r0788048@users.noreply.github.com>
2021-03-05 15:56:39 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import pymongo as mongo
 import requests
 import time
+import redis
 
 def scrape():
     url = 'https://www.blockchain.com/btc/unconfirmed-transactions'
@@ -11,14 +12,17 @@ def scrape():
 
     has = soup.find_all('a',{ "class" : "sc-1r996ns-0 fLwyDF sc-1tbyx6t-1 kCGMTY iklhnl-0 eEewhk d53qjk-0 ctEFcK"})
     allebs = soup.find_all('span',{ "class" : "sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC"})
-
+    	
     tel = 0
     for x in range(0, len(allebs)):
         if(x%3 == 1):
+            redisstring =  allebs[x-1].text + ' | ' + allebs[x].text + ' | ' + allebs[x+1].text
+            r = redis.Redis()
+            r.set(has[int((x-1)/3)].text, redisstring, ex=59)
             if (float(allebs[x].text[:-4]) > float(allebs[tel].text[:-4])):
                 tel = x
     
-    string = {"hash": has[(tel-1)%3].text, "time": allebs[tel-1].text, "am_btc": allebs[tel].text, "am_usd": allebs[tel+1].text}
+    string = {"hash": has[int((tel-1)/3)].text, "time": allebs[tel-1].text, "am_btc": allebs[tel].text, "am_usd": allebs[tel+1].text}
     x =  col_transactions.insert_one(string)
 
 client = mongo.MongoClient("mongodb://127.0.0.1:27017")
